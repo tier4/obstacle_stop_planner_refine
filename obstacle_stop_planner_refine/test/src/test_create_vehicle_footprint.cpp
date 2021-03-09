cf160d9eb3971d62acc37d18edcce368f05520b9
Keisuke Shima <keisuke.shima@tier4.jp>
2021-03-09 18:17:09 +0900

    Add test

---
+++ obstacle_stop_planner_refine/test/src/test_create_vehicle_footprint.cpp
@@ -0,0 +1,34 @@
+// Copyright 2021 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+#include "obstacle_stop_planner/util/create_vehicle_footprint.hpp"
+
+namespace obstacle_stop_planner
+{
+TEST(createVehicleFootprint, returnValue) {
+  VehicleInfo vehicle_info;
+  vehicle_info.front_overhang = 0.5;
+  vehicle_info.rear_overhang = 0.3;
+  vehicle_info.wheel_base = 2.0;
+  vehicle_info.wheel_tread = 0.1;
+  vehicle_info.left_overhang = 0.2;
+  vehicle_info.right_overhang = 0.2;
+
+  const auto ring2d = createVehicleFootprint(vehicle_info);
+  EXPECT_EQ(ring2d.size(), 5);
+  EXPECT_EQ(ring2d.at(0).x(), 2.5);
+  EXPECT_EQ(ring2d.at(0).y(), 0.25);
+}
+}  // namespace obstacle_stop_planner
