38e6ba5f1d107389600d99e8cde33319259cab83
Kenji Miyake <kenji.miyake@tier4.jp>
2021-03-22 09:19:48 +0900

    Install mkdocs
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

---
+++ scripts/install_mkdocs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pip3 install \
+    fontawesome_markdown \
+    markdown \
+    mdx_unimoji \
+    mkdocs \
+    mkdocs-material \
+    plantuml-markdown \
+    pymdown-extensions \
+    python-markdown-math
