f374c9d68a41937e7270b6eeb37ab0d3ae2e34d8
Keisuke Shima <keisuke.shima@tier4.jp>
2021-04-12 15:05:38 +0900

    fix visibility_control

--- obstacle_stop_planner_refine/include/obstacle_stop_planner/visibility_control.hpp
+++ obstacle_stop_planner_refine/include/obstacle_stop_planner/visibility_control.hpp
@@ -15,23 +15,35 @@
 #ifndef OBSTACLE_STOP_PLANNER__VISIBILITY_CONTROL_HPP_
 #define OBSTACLE_STOP_PLANNER__VISIBILITY_CONTROL_HPP_
 
-////////////////////////////////////////////////////////////////////////////////
-#if defined(__WIN32)
-  #if defined(OBSTACLE_STOP_PLANNER_BUILDING_DLL) || defined(OBSTACLE_STOP_PLANNER_EXPORTS)
-    #define OBSTACLE_STOP_PLANNER_PUBLIC __declspec(dllexport)
-    #define OBSTACLE_STOP_PLANNER_LOCAL
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define OBSTACLE_STOP_PLANNER_EXPORT __attribute__ ((dllexport))
+    #define OBSTACLE_STOP_PLANNER_IMPORT __attribute__ ((dllimport))
   #else
-    #define OBSTACLE_STOP_PLANNER_PUBLIC __declspec(dllimport)
+    #define OBSTACLE_STOP_PLANNER_EXPORT __declspec(dllexport)
+    #define OBSTACLE_STOP_PLANNER_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef OBSTACLE_STOP_PLANNER_BUILDING_LIBRARY
+    #define OBSTACLE_STOP_PLANNER_PUBLIC OBSTACLE_STOP_PLANNER_EXPORT
+  #else
+    #define OBSTACLE_STOP_PLANNER_PUBLIC OBSTACLE_STOP_PLANNER_IMPORT
+  #endif
+  #define OBSTACLE_STOP_PLANNER_PUBLIC_TYPE OBSTACLE_STOP_PLANNER_PUBLIC
+  #define OBSTACLE_STOP_PLANNER_LOCAL
+#else
+  #define OBSTACLE_STOP_PLANNER_EXPORT __attribute__ ((visibility("default")))
+  #define OBSTACLE_STOP_PLANNER_IMPORT
+  #if __GNUC__ >= 4
+    #define OBSTACLE_STOP_PLANNER_PUBLIC __attribute__ ((visibility("default")))
+    #define OBSTACLE_STOP_PLANNER_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define OBSTACLE_STOP_PLANNER_PUBLIC
     #define OBSTACLE_STOP_PLANNER_LOCAL
   #endif
-#elif defined(__linux__)
-  #define OBSTACLE_STOP_PLANNER_PUBLIC __attribute__((visibility("default")))
-  #define OBSTACLE_STOP_PLANNER_LOCAL __attribute__((visibility("hidden")))
-#elif defined(__APPLE__)
-  #define OBSTACLE_STOP_PLANNER_PUBLIC __attribute__((visibility("default")))
-  #define OBSTACLE_STOP_PLANNER_LOCAL __attribute__((visibility("hidden")))
-#else
-  #error "Unsupported Build Configuration"
+  #define OBSTACLE_STOP_PLANNER_PUBLIC_TYPE
 #endif
 
 #endif  // OBSTACLE_STOP_PLANNER__VISIBILITY_CONTROL_HPP_
